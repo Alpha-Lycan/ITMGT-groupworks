912f40e0266940bf80f6dbb7d571ee1d94e049c4
Alpha-Lycan <107444198+Alpha-Lycan@users.noreply.github.com>
2022-07-14 18:47:05 +0800

    next commit for proj

--- capstone project/capstone-project.py
+++ capstone project/capstone-project.py
@@ -1,24 +1,48 @@
 
-import pandas as pd
-import numpy as np
+from turtle import position
+from pymongo import MongoClient
 scp_dictlist=[]
+
+#mongodb connection thingy: mongodb+srv://Alpha_Lycan:<password>@cluster0.2tif2nh.mongodb.net/test
+
 
 entry_title=""
 date=""
-amount=0
+amount_cib=0
 flow_type=""
 category=""
 
-for i in range(2):
-    entry_title=input("Name of the activity:")
-    date=input("Date:")
-    amount=int(input("amount of cash used in the activity:"))
-    flow_type=input("Inflow or outflow checkboxes:")
-    category=input("Operating, Investing, or Financing checkboxes:") 
-    scp_dictlist.append({"Entry Title":entry_title, "Date":date, "Amount of Cash":int(amount),"Inflow or Outflow":flow_type,"Category":category})
+# for i in range(2):
+#     entry_title=input("Name of the activity:")
+#     date=input("Date:")
+#     amount=int(input("amount of cash used in the activity:"))
+#     flow_type=input("Inflow or outflow checkboxes:")
+#     category=input("Operating, Investing, or Financing checkboxes:") 
+#     scp_dictlist.append({"Entry Title":entry_title, "Date":date, "amount":int(amount_cib),"Inflow or Outflow":flow_type,"category":category})
 
 #cib=cash in bank from previous day
 #return the scp 
+
+def add_entry():
+    client=MongoClient("mongodb+srv://Alpha_Lycan:Charing@cluster0.2tif2nh.mongodb.net/test")
+    db=client["scp_database"]
+    mycol=db["daily_cash_position"]
+    mydict = {"entry_title":"bank investment","date":"","amount":100,"flow_type":"Outflow","category":"Investing"}
+    added_entry=mycol.insert_one(mydict)
+    #filter by title 
+    #filter is the first paremater and the second parameter is the return. 0 means u dont return it, 1 means u return it.
+    # for entry in entry_list:
+add_entry()
+
+def update_entry():
+    client=MongoClient("mongodb+srv://Alpha_Lycan:Charing@cluster0.2tif2nh.mongodb.net/test")
+    db=client["scp_database"]
+    mycol=db["daily_cash_position"]
+    myquery = { "entry_title": "food" }
+    newvalues = { "$set": { "flow_type": "Inflow" } }
+    mycol.update_one(myquery, newvalues) 
+    
+update_entry()   
 
 def compute_values(scp_dictlist,cib_prevday):
     #operating
@@ -33,26 +57,26 @@ def compute_values(scp_dictlist,cib_prevday):
     fin_in=0
     fin_out=0
     net_fin=0
-    #cib computation
+    #cash in bank computation
     net_in_cib=0
     cib_today=0
 
     for entry in scp_dictlist:
-        if entry["Category"]=="Operating":
-            if flow_type=="Inflow":
-                op_in+=entry["Amount of Cash"]
+        if entry["category"]=="Operating":
+            if entry["flow_type"]=="Inflow":
+                op_in+=entry["amount"]
             else:
-                op_out+=entry["Amount of Cash"]
-        elif entry["Category"]=="Investing":
-            if flow_type=="Inflow":
-                inv_in+=entry["Amount of Cash"]
+                op_out+=entry["amount"]
+        elif entry["category"]=="Investing":
+            if entry["flow_type"]=="Inflow":
+                inv_in+=entry["amount"]
             else:
-                inv_out+=entry["Amount of Cash"]
-        elif entry["Category"]=="Financing":
-            if flow_type=="Inflow":
-                fin_in+=entry["Amount of Cash"]
+                inv_out+=entry["amount"]
+        elif entry["category"]=="Financing":
+            if entry["flow_type"]=="Inflow":
+                fin_in+=entry["amount"]
             else:
-                fin_out+=entry["Amount of Cash"]
+                fin_out+=entry["amount"]
     net_op=op_in-op_out
     net_inv=inv_in-inv_out
     net_fin=fin_in-fin_out
@@ -60,9 +84,29 @@ def compute_values(scp_dictlist,cib_prevday):
     cib_today=cib_prevday+net_in_cib
     return(op_in,op_out,net_op,inv_in,inv_out,net_inv,fin_in,fin_out,net_fin,net_in_cib,cib_today)
 
+#delete query
+
+def delete_entry():
+    client=MongoClient("mongodb+srv://Alpha_Lycan:Charing@cluster0.2tif2nh.mongodb.net/test")
+    db=client["scp_database"]
+    mycol=db["daily_cash_position"]
+    myquery = { "entry_title": "bank investment" }
+    mycol.delete_one(myquery)
+delete_entry()
+
+
+def get_entry():
+    client=MongoClient("mongodb+srv://Alpha_Lycan:Charing@cluster0.2tif2nh.mongodb.net/test")
+    db=client["scp_database"]
+    mycol=db["daily_cash_position"]
+    entry_list=mycol.find({},{"_id":0,"entry_title":1,"date":1,"amount":1,"flow_type":1,"category":1,})
+    #filter by title 
+    #filter is the first paremater and the second parameter is the return. 0 means u dont return it, 1 means u return it.
+    # for entry in entry_list:
+    return(entry_list)
+scp_dictlist=get_entry()
+
 res=compute_values(scp_dictlist,83275)
 print(res)
-
-
 
 
